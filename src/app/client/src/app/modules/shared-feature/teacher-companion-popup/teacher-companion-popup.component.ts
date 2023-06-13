3d97d535cd6254e626deeebc107056484ef9d320
princegupta1131 <114015020+princegupta1131@users.noreply.github.com>
2023-06-13 15:24:09 +0530

    implemented teachmate feature for textbook

--- src/app/client/src/app/modules/shared-feature/teacher-companion-popup/teacher-companion-popup.component.ts
+++ src/app/client/src/app/modules/shared-feature/teacher-companion-popup/teacher-companion-popup.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, Inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject, Subscription, throwError } from 'rxjs';
@@ -13,9 +13,11 @@ import * as _ from 'lodash-es';
   templateUrl: './teacher-companion-popup.component.html',
   styleUrls: ['./teacher-companion-popup.component.scss'],
 })
-export class TeacherCompanionPopupComponent implements OnInit {
+export class TeacherCompanionPopupComponent implements OnInit, AfterViewInit {
+  @ViewChild('scrollContainer', { static: false }) scrollContainer: ElementRef;
 
-  constructor(private dialogRef: MatDialogRef<TeacherCompanionPopupComponent>, private http: HttpClient, private userService: UserService, @Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(private dialogRef: MatDialogRef<TeacherCompanionPopupComponent>, private http: HttpClient, private userService: UserService, @Inject(MAT_DIALOG_DATA) public data: any,
+  ) { }
   firstPage: boolean;
   secondPage: boolean;
   selectedOption: string = '';
@@ -30,14 +32,21 @@ export class TeacherCompanionPopupComponent implements OnInit {
   public unsubscribe$ = new Subject<void>();
   userProfile: any;
   userName; any;
+  contentHeight;
 
-  options = [{}];
+  options = [
+    {}
+  ];
 
   texbookIdConfig =
     {
-      "do_2135989935565701121331": "4c67c7f4-0919-11ee-9081-0242ac110002"
+      "do_2138168881245880321735": "4c67c7f4-0919-11ee-9081-0242ac110002"
     }
 
+  conversion = [
+
+
+  ];
   ngOnInit() {
     this.options = this.data.children
     this.uuid = this.texbookIdConfig[this.data.identifier];
@@ -46,6 +55,7 @@ export class TeacherCompanionPopupComponent implements OnInit {
     this.thirdPage = false;
     this.getUserDetails()
   }
+
   ngOnDestroy() {
     if (this.userSubscription) {
       this.userSubscription.unsubscribe();
@@ -53,6 +63,25 @@ export class TeacherCompanionPopupComponent implements OnInit {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
   }
+  scrollToBottom() {
+    // const scrollContainer = this.scrollContainer.nativeElement;
+    // scrollContainer.scrollTop = scrollContainer.scrollHeight;
+    var test = this.contentHeight != this.scrollContainer.nativeElement.scrollHeight 
+    //&& this.contentRef.nativeElement.scrollHeight != (this.contentRef.nativeElement.scrollTop + this.contentRef.nativeElement.offsetHeight);
+  console.log(test);
+
+  if (this.contentHeight != this.scrollContainer.nativeElement.scrollHeight && this.scrollContainer.nativeElement.scrollHeight != (this.scrollContainer.nativeElement.scrollTop + this.scrollContainer.nativeElement.offsetHeight)){
+    this.scrollContainer.nativeElement.scrollTo(0, this.scrollContainer.nativeElement.scrollHeight);
+  }
+  }
+  ngAfterViewInit() {
+    var test = this.contentHeight != this.scrollContainer.nativeElement.scrollHeight 
+    //&& this.contentRef.nativeElement.scrollHeight != (this.contentRef.nativeElement.scrollTop + this.contentRef.nativeElement.offsetHeight);
+  console.log(test);
+
+  if (this.contentHeight != this.scrollContainer.nativeElement.scrollHeight && this.scrollContainer.nativeElement.scrollHeight != (this.scrollContainer.nativeElement.scrollTop + this.scrollContainer.nativeElement.offsetHeight)){
+    this.scrollContainer.nativeElement.scrollTo(0, this.scrollContainer.nativeElement.scrollHeight);
+  }  }
 
   getUserDetails() {
     this.userSubscription = this.userService.userData$.subscribe((user: any) => {
@@ -66,11 +95,22 @@ export class TeacherCompanionPopupComponent implements OnInit {
 
   toGetApiResponse(querParms, uuid): Observable<any> {
     this.isLoading = false;
-    const url = `http://4.224.41.213:9000/query-with-langchain-gpt4?uuid_number=${uuid}&query_string=${encodeURIComponent(querParms)}`;
+    // const url = `http://4.224.41.213:9000/query-with-langchain-gpt4?uuid_number=${uuid}&query_string=${encodeURIComponent(querParms)}`;
+    const url = `http://20.244.48.128:8000/query-with-langchain-gpt4?uuid_number=${uuid}&query_string=${encodeURIComponent(querParms)}`;
     return this.http.get(url).pipe(
       map((data: any) => {
         this.apiResData = data.answer;
         console.log(this.apiResData);
+        let resObject =
+        {
+          index: this.conversion.length + 1,
+          question: data.query,
+          response: data.answer
+
+        }
+        this.conversion.push(resObject);
+          // this.scrollToBottom();
+        this.contentHeight = this.scrollContainer.nativeElement.scrollHeight;
         this.isLoading = true;
         return data; // Optional: Return the data if needed
       }),
@@ -100,7 +140,16 @@ export class TeacherCompanionPopupComponent implements OnInit {
     this.secondPage = false;
     this.toGetApiResponse(this.searchQuery, this.uuid).subscribe(data => {
       this.bookDescription = data.answer;
+      // let resObject =
+      // {
+      //   index: this.conversion.length + 1,
+      //   question: data.query,
+      //   response: data.answer
+
+      // }
+      // this.conversion.push(resObject);
     });
+    console.log(this.conversion);
     // Handle the search logic here
     console.log('Search query:', this.searchQuery);
   }
@@ -110,25 +159,54 @@ export class TeacherCompanionPopupComponent implements OnInit {
     this.secondPage = false;
     if (val === "Quiz") {
       console.log(val)
-      let Quiz = 'can you please give me the top 5 question for this ' + this.chapterTilte;
+      let Quiz = 'Generate 5 MCQ for this ' + this.chapterTilte;
       this.toGetApiResponse(Quiz, this.uuid).subscribe(data => {
         this.bookDescription = data.answer;
+        // let resObject =
+        // {
+        //   index: this.conversion.length + 1,
+        //   question: data.query,
+        //   response: data.answer
+
+        // }
+        // this.conversion.push(resObject);
 
       });
+      console.log(this.conversion);
+
     }
     else if (val === "Summary") {
       console.log(val)
-      this.toGetApiResponse('Summarize on ' + this.chapterTilte, this.uuid).subscribe(data => {
+      this.toGetApiResponse('Summarize ' + this.chapterTilte, this.uuid).subscribe(data => {
         this.bookDescription = data.answer;
+        // let resObject =
+        // {
+        //   index: this.conversion.length + 1,
+        //   question: data.query,
+        //   response: data.answer
+
+        // }
+        // this.conversion.push(resObject);
 
       });
+      console.log(this.conversion);
+
     }
     else if (val === "Material") {
       console.log(val)
-      this.toGetApiResponse('how to teach ' + this.chapterTilte, this.uuid).subscribe(data => {
+      this.toGetApiResponse('how to teach ' + this.chapterTilte + ' with activities', this.uuid).subscribe(data => {
         this.bookDescription = data.answer;
+        // let resObject =
+        // {
+        //   index: this.conversion.length + 1,
+        //   question: data.query,
+        //   response: data.answer
+
+        // }
+        // this.conversion.push(resObject);
 
       });
+      console.log(this.conversion);
     }
     // this.bookDescription = "Friction is a force that opposes the relative motion or tendency of such motion between two surfaces in contact with each other. It acts on both surfaces and depends on the nature and smoothness of the surfaces. Friction can be static (when the object is at rest), sliding (when an object is sliding over another), or rolling (when one body rolls over another). Friction is essential for many everyday activities like walking, gripping objects, and stopping vehicles, but it can also cause wear, tear, and energy loss in some situations";
 
